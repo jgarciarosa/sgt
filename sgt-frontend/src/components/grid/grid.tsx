5b76e4f6993cdfec7a5bb76a8d1cb8a1422acbfb
jgarciarosa <jgarcia.rosa@gmail.com>
2024-10-21 09:36:18 -0300

    Limpando arquivos e criando component Grid.

---
+++ sgt-frontend/src/components/grid/grid.tsx
@@ -0,0 +1,12 @@
+const Grid: React.FC = () => {
+
+    return (
+        <>
+            <div>
+                <h1>Sistema de Gerenciamento de Tarefas</h1>
+            </div>
+        </>
+    )
+}
+
+export default Grid;
