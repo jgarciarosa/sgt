cff41abda4664e7274ec7915dcbcffd4991c7bcc
jgarciarosa <jgarcia.rosa@gmail.com>
2024-10-20 23:44:52 -0300

    Criando backend

---
+++ sgt-backend/src/main/java/br/com/jgarciarosa/sgt/SgtApplication.java
@@ -0,0 +1,13 @@
+package br.com.jgarciarosa.sgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgtApplication.class, args);
+	}
+
+}
